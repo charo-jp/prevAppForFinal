0b3822b8e1f49c433ba4402d38e3d7fd5e386542
jbkh2 <jbkh2@kent.ac.uk>
2022-07-30 12:13:04 +0100

    registration page can now send json object of registration data to server, not to localhost yet. Made it posible to get value of checkbbox to be of a boolean value

--- src/regForm/src/js/regForm.js
+++ src/regForm/src/js/regForm.js
@@ -1,107 +1,47 @@
 
-// // if checkbox for special resources is checked then show input if not hide
-// $('#specialResourcesCheck').click(function() {
-//     let isChecked = $('#specialResourcesCheck').is(':checked');
-//     if(isChecked) {
-//         $('#specialResInput').show();
-//     }else{
-//         $('#specialResInput').hide();
-//     }
-// });
+// if checkbox for special resources is checked then show input if not hide
+$('#specialResourcesCheck').click(function() {
+    let isChecked = $('#specialResourcesCheck').is(':checked');
+    if(isChecked) {
+        $('#specialResInput').show();
+    }else{
+        $('#specialResInput').hide();
+    }
+});
 
-// // if checkbox for ethics review is checked then show input if not hide
-// $('#ethicsReviewCheck').click(function() {
-//     let isChecked = $('#ethicsReviewCheck').is(':checked');
-//     if(isChecked) {
-//         $('#ethicsReviewInput').show();
-//         $('#checkList').show();
-//     }else{
-//         $('#ethicsReviewInput').hide();
-//         $('#checkList').hide();
-//     }
+// if checkbox for ethics review is checked then show input if not hide
+$('#ethicsReviewCheck').click(function() {
+    let isChecked = $('#ethicsReviewCheck').is(':checked');
+    if(isChecked) {
+        $('#ethicsReviewInput').show();
+        $('#checkList').show();
+    }else{
+        $('#ethicsReviewInput').hide();
+        $('#checkList').hide();
+    }
 
-// });
+});
 
-
-// ///testing
-// let rawName = $('#nameInput');
-    
-//     // let name = document.querySelector('#name');
-//     // let email = document.querySelector('#email');
-
-//     let name = rawName.val('john test');
-
-
-//     // Creating a XHR object
-    
-//     let url = "form.php";
-
-//     const toSend = {
-//         studentName: rawName.val(),
-//     };
-//     console.log(toSend);
-    
-
-//     const data = JSON.stringify(toSend);
-//     console.log(data);
-
-//     let xhr = new XMLHttpRequest();
-
-//     //open a connection
-//     xhr.open("POST", 'form.php');
-
-//     //Set the request header of type json
-//     xhr.setRequestHeader("Content-Type", "application/json");
-//     xhr.send(data);
-
-// ///endTesting
-
-
-// function sendJSON(){
-
-//     // let rawName = $('#nameInput');
-    
-//     // // let name = document.querySelector('#name');
-//     // // let email = document.querySelector('#email');
-
-//     // let name = rawName.val();
-
-//     const toSend = {
-//         name:$('nameInput'),
-//         login:$('loginInput')
-//     }
-
-//     // Converting JSON data to string
-//     const data = JSON.stringify(toSend);
-
-//     // Creating a XHR object
-//     const xhr = new XMLHttpRequest();
-//     // let url = "submit.php";
-
-//     // Create a state change callback
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === 4 && xhr.status === 200) {
-
-//             // Print received data from server
-//             result.innerHTML = this.responseText;
-
-//         }
-//     };
-
-//     // open a connection
-//     xhr.open("POST", "submit.php");
-
-//     // Set the request header of type json
-//     xhr.setRequestHeader("Content-Type", "application/json");
-//     // Sending data with the request
-//     xhr.send(data);
-// }
 
 //https://www.geeksforgeeks.org/how-to-send-a-json-object-to-a-server-using-javascript/
 function sendJSON(){
+    //storing input variables
     let result = document.querySelector('.result');
-    let name = document.querySelector('#name');
-    let login = document.querySelector('#login');
+    let name = document.querySelector('#nameInput');
+    let courseTitle = document.querySelector('#courseTitle');
+    let projectName = document.querySelector('#projectName');
+    let supervisorName = document.querySelector('#supervisorName');
+    let cyberSec = $('#cybersecProject'); //checkbox
+    let specialRes = $('#specialResourcesCheck'); //checkbox
+    let specialResInput = document.querySelector('#specialResInput');
+
+    let ethics = $('#ethicsReviewCheck'); //checkbox
+    let ethicsReviewInput = document.querySelector('#ethicsReviewInput');
+
+    //dealing with getting checkbox vlaues
+    let ethicsReviewCheck = ethics.prop('checked');
+    let specialResourcesCheck = specialRes.prop('checked');
+    let cybersecProject = cyberSec.prop('checked');
       
     // Creating a XHR object
     let xhr = new XMLHttpRequest();
@@ -123,8 +63,23 @@ function sendJSON(){
         }
     };
 
-    // Converting JSON data to string
-    var data = JSON.stringify({ "name": name.value, "login": login.value });
+    // Converting JSON data to string 
+    var data = JSON.stringify(
+        { 
+            "name": name.value,
+             "courseTitle": courseTitle.value,
+             "projectName": projectName.value,
+             "supervisorName": supervisorName.value,
+             "cybersecProject": cybersecProject, //checkbox
+             "specialResourcesCheck": specialResourcesCheck,//checkbox
+             "specialResInput": specialResInput.value,
+
+             "ethicsReviewCheck": ethicsReviewCheck, //checkbox
+             "ethicsReviewInput": ethicsReviewInput.value,
+             "ethicsReviewInput": ethicsReviewInput.value,
+             'groupProject': 0,
+        }
+    );
 
     // Sending data with the request
     xhr.send(data);
--- src/regForm/src/submit.php
+++ src/regForm/src/submit.php
@@ -4,7 +4,11 @@ header("Content-Type: application/json");
  
 //Decodes json into php variable
 $data = json_decode(file_get_contents("php://input"));
- 
-echo "Hello $data->name, your login is $data->login";
+echo "<div>
+        <p></p>
+        <p>Hello $data->name, your registration for $data->projectName has been sent</p>
+        <p></p>
+     </div>";
+// echo " ";
 
 ?>
