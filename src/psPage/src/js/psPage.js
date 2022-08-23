f6ea14aeb413ba391941bd7087a2282697397045
Jordan Duong <jd647@kent.ac.uk>
2022-08-23 15:59:26 +0100

    Added button to generate report on all

--- src/psPage/src/js/psPage.js
+++ src/psPage/src/js/psPage.js
@@ -141,3 +141,51 @@ function upload() {
     document.body.removeChild(link);
   
   }
+
+  async function getAll() {
+    let url = 'http://localhost:8080/getusers';
+    let response = await fetch(url);
+    var data = await response.json();
+    jsonArray = [];
+
+    for( i =0; i < data.length; i++) {
+          jsonArray.push(data[i]);
+    }
+
+    if (jsonArray.length == 0) {
+      alert("Data does not exist");
+      return;
+    }
+
+    var excel = 'sep=,' + '\r\n\n';
+    var value = "";
+    for (var index in jsonArray[0]) {
+        value += index + ',';
+    }
+    value = value.slice(0, -1);
+    excel += value + '\r\n';
+    
+    //Double for loop to extract specific values
+    for (var i = 0; i < jsonArray.length; i++) {
+        var value = "";
+        for (var index in jsonArray[i]) {
+            value += '"' + jsonArray[i][index] + '",';
+        }
+        value.slice(0, value.length - 1);
+        excel += value + '\r\n';
+    }
+    
+    //Create element to link file to
+    var link = document.createElement("a");    
+    // Convert excel into excel file and link to element
+    link.href = 'data:text/csv;charset=utf-8,' + escape(excel);
+    
+    //Hide element and use to download file automatically
+    link.style = "visibility:hidden";
+    link.download = "data.csv";
+    
+    //Remove element after downloading the file
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
